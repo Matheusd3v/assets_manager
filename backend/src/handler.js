217e5801a199b6ed67cb2dd23a246fcc7e2d1482
Matheus <>
2024-08-01 22:51:14 -0300

    feat: apply route manager and error handler in main function

--- backend/src/handler.js
+++ backend/src/handler.js
@@ -1,16 +1,11 @@
+const { routeManager } = require("./shared/route-manager.shared");
+const ExceptionHanlder = require("./shared/exceptions/handler.exception")
+
 module.exports.main = async (event) => {
-  const [method, route] = event.routeKey.split(" ");
-
-  let response = { statusCode: 404 };
-
-  if (route === "/example" && method === "GET") {
-    const example = require("./functions/example");
-    response = await example();
-  } 
-  // else if (route === "/example2" && method === "POST") {
-  //   const example = require("./functions/example2");
-  //   response = await example();
-  // }
-
-  return response;
+    try {
+        const response = await routeManager.handleRequest(event);
+        return response
+    } catch (error) {
+        return ExceptionHanlder.catch(error)
+    }
 };
