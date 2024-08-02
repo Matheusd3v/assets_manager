8f5db22fe1b002018c645c79f93dbafb460c0ba6
Matheus <>
2024-08-01 22:56:33 -0300

    feat: add sensor mapper

---
+++ backend/src/sensors/mapper/sensor.mapper.js
@@ -0,0 +1,45 @@
+const { Sensor } = require('../sensor.entity')
+
+class SensorPresentation {
+    id
+    name
+    assetId
+}
+
+class SensorBodyCreate {
+    name
+    assetId
+}
+
+class SensorMapper {
+    /**
+     * 
+     * @param {SensorBodyCreate} createBody 
+     * @returns {Sensor}
+     */
+    static toEntity(createBody) {
+        const sensor = new Sensor(
+            createBody.name,
+            createBody.assetId
+        )
+        return sensor
+    }
+
+    /**
+     * 
+     * @param {Sensor} sensor 
+     * @returns {SensorPresentation}
+     */
+    static toClient(sensor) {
+        return {
+            id: sensor._id,
+            name: sensor._name,
+            assetId: sensor._assetId
+        }
+    }
+}
+
+module.exports = {
+    SensorMapper,
+    SensorPresentation
+}
