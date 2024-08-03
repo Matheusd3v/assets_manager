ff9bc413e73253be1195987b7ab4cd302fb20a4a
Matheus <>
2024-08-02 21:53:57 -0300

    feat: add collection controller

--- backend/src/assets/usecase/create-asset.usecase.js
+++ backend/src/assets/usecase/create-asset.usecase.js
@@ -1,17 +1,23 @@
-const { IAssetRepository } = require('../repository/asset-interface.repository')
-const { AssetMapper } = require('../mapper/asset.mapper')
+const {
+    IAssetRepository,
+} = require("../repository/asset-interface.repository");
+const { AssetMapper } = require("../mapper/asset.mapper");
 
 module.exports = class CreateAssetUseCase {
     /**
-     * 
-     * @param {IAssetRepository} assetRepository 
+     *
+     * @param {IAssetRepository} assetRepository
      */
     constructor(assetRepository) {
-        this._assetRepository = assetRepository
+        this._assetRepository = assetRepository;
     }
 
+    /**
+     *
+     * @param {{ name: string }} body
+     */
     async execute(body) {
-        const asset = AssetMapper.toEntity(body)
-        await this._assetRepository.create(asset)
+        const asset = AssetMapper.toEntity(body);
+        await this._assetRepository.create(asset);
     }
-}
+};
---
+++ backend/src/collections/colletion.controller.js
@@ -0,0 +1,98 @@
+const {
+    assetInMemoryRepository,
+} = require("../assets/repository/asset-in-memory.repository");
+const GetAssetByIdUseCase = require("../assets/usecase/get-asset-by-id.usecase");
+const {
+    sensorInMemoryRepository,
+} = require("../sensors/repository/sensor-in-memory.repository");
+const GetSensorByIdUseCase = require("../sensors/usecase/get-sensor-by-id.usecase");
+const GetSensorUseCase = require("../sensors/usecase/get-sensor.usecase");
+const { HttpStatus, HttpMethod } = require("../shared/enum.shared");
+const {
+    collectionInMemoryRepository,
+} = require("./repository/collection-in-memory.repository");
+const CreateCollectionUseCase = require("./usecase/create-collection.usecase");
+const DeleteCollectionUseCase = require("./usecase/delete-collection.usecase");
+const GetCollectionsUseCase = require("./usecase/get-collections.usecase");
+
+const CollectionRoute = {
+    CREATE: "/assets/{assetId}/sensors/{sensorId}",
+    LIST: "/assets/{assetId}/sensors/{sensorId}",
+};
+
+class CollectionController {
+    static routes = [
+        {
+            method: HttpMethod.GET,
+            route: CollectionRoute.LIST,
+            func: CollectionController.list,
+        },
+        {
+            method: HttpMethod.POST,
+            route: CollectionRoute.CREATE,
+            func: CollectionController.create,
+        },
+    ];
+
+    static async list({ params }) {
+        const sensorId = +params.sensorId;
+        const assetId = +params.assetId;
+        const getAssetByIdUseCase = new GetAssetByIdUseCase(
+            assetInMemoryRepository
+        );
+        const getSensorByIdUseCase = new GetSensorByIdUseCase(
+            sensorInMemoryRepository
+        );
+        const getCollectionsUseCase = new GetCollectionsUseCase(
+            collectionInMemoryRepository
+        );
+
+        await Promise.all([
+            getAssetByIdUseCase.execute(assetId),
+            getSensorByIdUseCase.execute(sensorId),
+        ]);
+        const collections = await getCollectionsUseCase.execute({
+            sensorId,
+        });
+
+        return {
+            statusCode: HttpStatus.OK,
+            body: {
+                data: collections,
+            },
+        };
+    }
+
+    static async create({ params, body }) {
+        const sensorId = +params.sensorId;
+        const assetId = +params.assetId;
+        const getAssetByIdUseCase = new GetAssetByIdUseCase(
+            assetInMemoryRepository
+        );
+        const getSensorByIdUseCase = new GetSensorByIdUseCase(
+            sensorInMemoryRepository
+        );
+        const createCollectionUseCase = new CreateCollectionUseCase(
+            collectionInMemoryRepository
+        );
+
+        await Promise.all([
+            getAssetByIdUseCase.execute(assetId),
+            getSensorByIdUseCase.execute(sensorId),
+        ]);
+        await createCollectionUseCase.execute({
+            value: body.value,
+            date: new Date(body.date),
+            sensorId,
+        });
+
+        return {
+            statusCode: HttpStatus.CREATED,
+            body: {
+                message: "Coleta criada com sucesso!",
+            },
+        };
+    }
+}
+
+module.exports = { CollectionController };
