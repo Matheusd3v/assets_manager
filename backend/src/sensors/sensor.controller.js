6efb5037e745eedd70d42153e3979f75a89b9b61
Matheus <>
2024-08-02 21:54:35 -0300

    feat: add delete collection usecase into sensor route

--- backend/src/sensors/sensor.controller.js
+++ backend/src/sensors/sensor.controller.js
@@ -9,6 +9,10 @@ const CreateSensorUseCase = require("./usecase/create-sensor.usecase");
 const GetSensorUseCase = require("./usecase/get-sensor.usecase");
 const GetAssetByIdUseCase = require("../assets/usecase/get-asset-by-id.usecase");
 const DeleteSensorUseCase = require("./usecase/delete-sensor.usecase");
+const DeleteCollectionUseCase = require("../collections/usecase/delete-collection.usecase");
+const {
+    collectionInMemoryRepository,
+} = require("../collections/repository/collection-in-memory.repository");
 
 const SensorRoute = {
     LIST: "/assets/{assetId}/sensors",
@@ -71,12 +75,27 @@ class SensorController {
         };
     }
 
-    static async delete({ params }) {
+    static async delete({ params, queryString }) {
         const assetId = +params.assetId;
         const sensorId = +params.sensorId;
+        const date = queryString?.date;
         const deleteSensorUseCase = new DeleteSensorUseCase(
             sensorInMemoryRepository
         );
+        if (sensorId && date) {
+            const deleteCollectionUseCase = new DeleteCollectionUseCase(
+                collectionInMemoryRepository
+            );
+            await deleteCollectionUseCase.execute({
+                date: new Date(date),
+                sensorId,
+            });
+
+            return {
+                statusCode: HttpStatus.NO_CONTENT,
+            };
+        }
+
         await deleteSensorUseCase.execute({
             id: sensorId,
             assetId,
@@ -84,7 +103,7 @@ class SensorController {
 
         return {
             statusCode: HttpStatus.NO_CONTENT,
-        }
+        };
     }
 }
 
