42d92c6454b00e15ca4cf5a82ee6d6ba61ce54ac
Matheus <>
2024-08-04 19:22:25 -0300

    chore: add react boilerplate

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+// import { RouteProvider } from './routes'
+
+function App() {
+
+  return (
+    <>
+      {/* <RouteProvider/> */}
+    </>
+  )
+}
+
+export default App
