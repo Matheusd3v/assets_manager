ff64a5f2e038e05dac96a590a6dd65b21f0bd37d
Matheus <>
2024-08-05 22:29:54 -0300

    feat: add global form style

---
+++ frontend/src/style/form-style.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+
+export const FormStyle = styled.form`
+    margin: 0 auto;
+    background-color: #FFF;
+    width: 300px;
+    height: 150px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    border-radius: 10px;
+    box-sizing: border-box;
+    padding: 5px 15px;;
+
+    button {
+        margin: 0 auto;
+    }
+
+    p {
+        color: red;
+        font-weight: 100;
+        font-size: 10px
+    }
+
+`
