a02e3dd471a2d763d7919c2da6ad4d1bf309b588
Matheus <>
2024-08-05 23:16:00 -0300

    fix: remove unused code

--- frontend/src/pages/Sensors/index.tsx
+++ frontend/src/pages/Sensors/index.tsx
@@ -30,23 +30,6 @@ export const SensorPage = () => {
     getCollections();
   }, []);
 
-  const option = {
-    xAxis: {
-      type: "category",
-      data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
-    },
-    yAxis: {
-      type: "value",
-    },
-    series: [
-      {
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-        type: "line",
-        smooth: true,
-      },
-    ],
-  };
-
   return (
     <DefaultContainerPageStyle>
       <header className="page-header">
