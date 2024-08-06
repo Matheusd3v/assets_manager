ace593418b72a1e548a316f52c742b9802bc0c68
Matheus <>
2024-08-05 22:37:52 -0300

    feat: implement toastify

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { RouteProvider } from './routes'
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <>
       <RouteProvider/>
+      <ToastContainer/>
     </>
   )
 }
