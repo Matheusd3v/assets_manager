9f76cc2f3f44ef3a5764c7016c6e3a688b04ecbb
Matheus <>
2024-08-05 22:36:09 -0300

    feat: add asset page

--- frontend/src/pages/Assets/index.tsx
+++ frontend/src/pages/Assets/index.tsx
@@ -1,6 +1,76 @@
+import { useParams } from "react-router";
+import { DefaultContainerPageStyle } from "../../style/DefaultContainerPageStyle";
+import { useEffect, useState } from "react";
+import { ISensor } from "../../types";
+import { api } from "../../services/api";
+import { Card } from "../../components/Card";
+import { ROUTES } from "../../routes/constants";
+import { Button } from "../../components/Button";
+import { Modal } from "../../components/Modal";
+import { EntityForm } from "../../components/EntityForm";
 
 export const AssetPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [sensorList, setSensorLit] = useState<ISensor[]>([]);
+  const { id } = useParams();
+  const apiUrlRegisterSensors = `assets/${id}/sensors`;
+  const nextPath = (sensorId: number) => {
+    const basePath = ROUTES.sensor.list.path;
+    return basePath
+      .replace("/:id", `/${id}`)
+      .replace("/:sensorId", `/${sensorId}`);
+  };
+
+  const getSensors = () => {
+    api.get(`/assets/${id}/sensors`).then((response) => {
+      setSensorLit(
+        response.data.sensors.map((sensor: ISensor) => {
+          sensor.totalRelation = sensor.totalCollections;
+          return sensor;
+        })
+      );
+    });
+  };
+
+  const deleteCard = (assetId: number, sensorId: number) => {
+    api.delete(`/assets/${assetId}/sensors/${sensorId}`).then(() => {
+      getSensors();
+    });
+  };
+
+  useEffect(() => {
+    getSensors();
+  }, []);
+
   return (
-    <div>AssetPage</div>
-  )
-}
+    <DefaultContainerPageStyle>
+      <header className="page-header">
+        <h2>Sensores</h2>
+
+        <Button size="medium" onClick={() => setIsOpen(true)}>
+          Cadastrar Sensor
+        </Button>
+      </header>
+
+      {isOpen && (
+        <Modal callback={getSensors} setIsOpen={setIsOpen}>
+          <EntityForm url={apiUrlRegisterSensors} />
+        </Modal>
+      )}
+
+      {sensorList.length > 0 ? (
+        sensorList.map((sensor) => (
+          <Card
+            callback={() => deleteCard(id as unknown as number, sensor.id)}
+            descriptionType="collection"
+            entity={sensor}
+            key={sensor.id}
+            path={nextPath(sensor.id)}
+          />
+        ))
+      ) : (
+        <p className="empty-message">Sem registros por aqui...</p>
+      )}
+    </DefaultContainerPageStyle>
+  );
+};
