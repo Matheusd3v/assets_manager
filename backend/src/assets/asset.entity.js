dd58b1cbed285837e3ad95dfcdf2f72c64ea48fa
Matheus <>
2024-08-05 21:05:02 -0300

    feat: add new field into asset entity

--- backend/src/assets/asset.entity.js
+++ backend/src/assets/asset.entity.js
@@ -2,15 +2,26 @@
 class Asset {
     _name
     _id
+    _totalSensors
 
     /**
      * 
      * @param {string} name 
      * @param {number} id 
+     * @param { number | undefined } totalSensors
      */
-    constructor(name, id) {
+    constructor(name, id, totalSensors) {
         this._name = name
         this._id = id
+        this._totalSensors = totalSensors
+    }
+
+    getTotalSensors() {
+        return this?._totalSensors ?? 0
+    }
+
+    setTotalSensors(total) {
+        this._totalSensors = total
     }
 }
 
