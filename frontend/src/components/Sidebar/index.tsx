b2790a0ffb6aaa58a549c4b85be253acb1408b9c
Matheus <>
2024-08-05 22:26:33 -0300

    feat: add sidebar component

--- frontend/src/components/Sidebar/index.tsx
+++ frontend/src/components/Sidebar/index.tsx
@@ -1,9 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { StyledBar } from "./style";
+import { Button } from "../Button";
+import { ROUTES } from "../../routes/constants";
 
 export const SideBar = () => {
+  const navigate = useNavigate();
   return (
     <>
-      <nav>SideBar</nav>
+      <StyledBar>
+        <img
+          src="https://ibbx.tech/wp-content/themes/ibbx/dist/images/logo.svg"
+          alt="IBBX logo"
+        />
+
+        <Button onClick={() => navigate(ROUTES.home.list.path)}>Home</Button>
+      </StyledBar>
       <Outlet />
     </>
   );
---
+++ frontend/src/components/Sidebar/style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledBar = styled.nav`
+  width: 15%;
+  height: 100vh;
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+  background-color: rgb(21, 23, 24);
+  box-shadow: rgba(0, 0, 0, 0.82) 0px 0px 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  padding: 20px 50px;
+  box-sizing: border-box;
+
+  img {
+    margin-top: 10px;
+    margin-bottom: 50px;
+  }
+`;
